89dc50e06ed088a5071c3bc6d50ff4dddb5cdae7
Harold Lee <harry.yg.lee@gmail.com>
2020-05-23 15:49:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,5 +49,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined)  {
+  return "Sorry, we don't have a credit card on file for you."
+} else {
+  let sum = total();
+  cart.splice(0);
+  return "Your total cost is $" + sum + ", which will be charged to the card " + cardNumber + ".";
+}
 }
