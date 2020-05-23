371d5fd9b80077191370d863f5e1ca58b2741108
Harold Lee <harry.yg.lee@gmail.com>
2020-05-23 15:30:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ return `${item} has been added to your cart.`
 function viewCart() {
 if (cart.length===0)
 return 'Your shopping cart is empty.'
+if(cart.length===0)
+return `In your cart, you have ${cart[0].itemName} at ${cart[0.itemPrice}
 }
 
 function total() {
